8e133643708c43fafb521615f3eb51e91ca75a07
Calico Rose Randall <139941083+codebycalico@users.noreply.github.com>
2026-05-29 11:42:58 -0700

    updating echoing emotions page and trying youtube embedding

--- technology/typewriter.js
+++ technology/typewriter.js
@@ -1,6 +1,8 @@
 /* 
   calico rose's website - createdbycalico.com
   typewriter effect for the headers on the technology page
+  references: https://www.w3schools.com/howto/howto_js_typewriter.asp
+  https://codepen.io/Lerke/pen/MWPOKZj
 */
 
 // select all document objects that match what's in the quotes (every h2 that has a data-text attribute)
